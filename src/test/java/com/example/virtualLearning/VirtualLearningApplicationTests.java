dc413fde25bc530d17974e9cacefe24c5602300f
sha-rath-ms <sharathsharu9155@gmail.com>
2022-02-07 16:59:57 +0530

    Initial commit with spring boot

---
+++ src/test/java/com/example/virtualLearning/VirtualLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.virtualLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
