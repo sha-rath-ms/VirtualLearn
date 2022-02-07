31d4590318dfecb534d8cfe125551a5a2306c58d
sheeba <sheebasufiyan@gmail.com>
2022-02-07 17:21:45 +0530

    Changed gradle files.

--- src/main/java/com/example/virtualLearning/VirtualLearningApplication.java
+++ src/main/java/com/example/virtualLearning/VirtualLearningApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VirtualLearningApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Virtual Learning");
 		SpringApplication.run(VirtualLearningApplication.class, args);
 	}
 
