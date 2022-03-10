6d2064a54bffe3620640cd2b076f37143a0c0ac3
sha-rath-ms <sharathsharu9155@gmail.com>
2022-03-10 18:32:02 +0530

    Final Commit

--- src/main/java/com/example/virtualLearning/controller/MyCourseController.java
+++ src/main/java/com/example/virtualLearning/controller/MyCourseController.java
@@ -1,5 +1,6 @@
 package com.example.virtualLearning.controller;
 
+import com.example.virtualLearning.response.GetTimeStamp;
 import com.example.virtualLearning.response.ResponseAllCourse;
 import com.example.virtualLearning.response.ResponseWrapper;
 import com.example.virtualLearning.response.ResultInfoConstants;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,47 +26,56 @@ public class MyCourseController {
 
     @PostMapping("/add")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseWrapper<String> add(@RequestHeader("course-id") Long courseId,@RequestHeader("Authorization") String token) {
+    public ResponseWrapper<String> add(@RequestHeader("course-id") Long courseId, @RequestHeader("Authorization") String token) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
-        myCourseService.addCourse(userId,courseId);
+        myCourseService.addCourse(userId, courseId);
         return new ResponseWrapper<String>(ResultInfoConstants.SUCCESS, null);
     }
 
     @GetMapping("/display-all")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseWrapper<List<ResponseAllCourse>> displayAll(@RequestHeader("Authorization") String token,@RequestParam(defaultValue = "0") Integer page) {
+    public ResponseWrapper<List<ResponseAllCourse>> displayAll(@RequestHeader("Authorization") String token, @RequestParam(defaultValue = "0") Integer page) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
-        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.getAllMyCourses(userId,page));
+        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.getAllMyCourses(userId, page));
     }
 
     @GetMapping("/display-completed")
     @ResponseStatus(HttpStatus.OK)
     public ResponseWrapper<List<ResponseAllCourse>> displayCompleted(@RequestHeader("Authorization") String token, @RequestParam(defaultValue = "0") Integer page) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
-        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS,   myCourseService.displayCompletedCourses(userId,page));
+        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.displayCompletedCourses(userId, page));
     }
 
     @GetMapping("/display-ongoing")
     @ResponseStatus(HttpStatus.OK)
     public ResponseWrapper<List<ResponseAllCourse>> displayOngoing(@RequestHeader("Authorization") String token, @RequestParam(defaultValue = "0") Integer page) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
-        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.displayOngoingCourses(userId,page));
+        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.displayOngoingCourses(userId, page));
     }
 
     @GetMapping("/display-certificate")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseWrapper<String> displayCertificate(@RequestHeader("course-id") Long courseId,@RequestHeader("Authorization") String token) {
+    public ResponseWrapper<String> displayCertificate(@RequestHeader("course-id") Long courseId, @RequestHeader("Authorization") String token) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
 
-        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.displayCertificate(userId,courseId));
+        return new ResponseWrapper<>(ResultInfoConstants.SUCCESS, myCourseService.displayCertificate(userId, courseId));
     }
+
     //
     @PutMapping("/set-completed")
     @ResponseStatus(HttpStatus.OK)
-    public ResponseWrapper setCompletion(@RequestHeader("course-id") Long courseId,@RequestHeader("Authorization") String token) {
+    public ResponseWrapper setCompletion(@RequestHeader("course-id") Long courseId, @RequestHeader("Authorization") String token) {
         Long userId = Long.parseLong(jwtUtility.getUserId(token));
-        myCourseService.updateCompleted(userId,courseId);
-        return new ResponseWrapper(ResultInfoConstants.SUCCESS,null );
+        myCourseService.updateCompleted(userId, courseId);
+        return new ResponseWrapper(ResultInfoConstants.SUCCESS, null);
+    }
+
+    @PutMapping("/save-status")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseWrapper save(@RequestHeader("Authorization") String token, @RequestHeader("course-id") long courseId, @RequestHeader("chapter-id") long chapterId, @RequestHeader("content-id") long contentId, @RequestBody @Valid GetTimeStamp getTimeStamp)
+    {
+        myCourseService.saveStatus(Long.parseLong(jwtUtility.getUserId(token)),courseId,chapterId,contentId,getTimeStamp);
+        return new ResponseWrapper(ResultInfoConstants.SUCCESS,null);
     }
 }
 
