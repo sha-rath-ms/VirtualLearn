41bbd8423a981fd2e4246e7caf07d16b1a5145af
sha-rath-ms <sharathsharu9155@gmail.com>
2022-03-10 21:52:38 +0530

    commited master

--- src/main/java/com/example/virtualLearning/response/ResponseWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.virtualLearning.response;
-
-import lombok.Data;
-
-@Data
-public class ResponseWrapper<T> {
-    private final ResultInfo resultInfo;
-    private final T data;
-}
--- src/main/java/com/example/virtualLearning/response/ResultInfo.java
+++
@@ -1,8 +0,0 @@
-package com.example.virtualLearning.response;
-
-import lombok.Data;
-
-@Data
-public class ResultInfo {
-    private final String message;
-}
